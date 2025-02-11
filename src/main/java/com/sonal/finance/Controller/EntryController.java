8903f0fa488237e0cab06eb92459deae89eeaa8e
Sonal Singh <sonalsinghh01@gmail.com>
2025-02-11 17:59:17 +0530

    adding controller for end points

--- src/main/java/com/sonal/finance/Controller/EntryController.java
+++ src/main/java/com/sonal/finance/Controller/EntryController.java
@@ -6,7 +6,6 @@ import com.sonal.finance.entity.EntryType;
 import com.sonal.finance.Service.EntryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
